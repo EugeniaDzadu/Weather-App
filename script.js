02670674e0e5fd72fcd4656b1a85fa375b2ed5a8
EugeniaDzadu <jeeniaace@gmail.com>
2024-11-12 11:11:01 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const locationName = document.querySelector('#location-name');
+const weatherMain = document.querySelector('#weather-main');
+const weatherDescription = document.querySelector('#weather-description');
+const weatherIcon = document.querySelector('#weather-icon');
+const temp = document.querySelector('#temp');
+const feelsLike = document.querySelector('#feels-like');
+const tempMin = document.querySelector('#temp-min');
+const tempMax = document.querySelector('#temp-max');
+const pressure = document.querySelector('#pressure');
+const humidity = document.querySelector('#humidity');
+const visibility = document.querySelector('#visibility');
+const cloudiness = document.querySelector('#cloudiness');
+const windSpeed = document.querySelector('#wind-speed');
+const windDeg = document.querySelector('#wind-deg');
+const windGust = document.querySelector('#wind-gust');
+const rain1h = document.querySelector('#rain-1h');
+const coordLon = document.querySelector('#coord-lon');
+const sunrise = document.querySelector('#sunrise');
+
+
+
